71dfe77a9e07569a2e6920d6c5acadfcd23098f1
ivan006 <ivan.copeland2015@gmail.com>
2021-02-07 12:26:08 +0200

    1

--- src/Controller/EntryController.php
+++ src/Controller/EntryController.php
@@ -48,7 +48,6 @@ class EntryController extends AbstractController
 
     $task = new Entry();
 
-
     $form = $this->createFormBuilder($task)
     ->add('name', TextType::class)
     ->add('email', TextType::class)
@@ -58,21 +57,12 @@ class EntryController extends AbstractController
 
     $form->handleRequest($request);
     if ($form->isSubmitted() && $form->isValid()) {
-      // $form->getData() holds the submitted values
-      // but, the original `$entry` variable has also been updated
+
       $entry = $form->getData();
 
-      // ... perform some action, such as saving the entry to the database
-      // for example, if Entry is a Doctrine entity, save it!
       $entityManager = $this->getDoctrine()->getManager();
       $entityManager->persist($entry);
       $entityManager->flush();
-
-      // $entry->setName($request->get('name'));
-      // $entry->setEmail($request->get('email'));
-      // $entry->setMessage($request->get('message'));
-      // $em->persist($entry);
-      // $em->flush();
 
       // return $this->redirectToRoute('entries');
       return $this->redirect('entry_success');
@@ -96,64 +86,4 @@ class EntryController extends AbstractController
 
   }
 
-  // /**
-  // * @Route("/entries", methods="POST")
-  // */
-  public function create(Request $request, EntryRepository $entryRepository, EntityManagerInterface $em)
-  {
-    $request = $this->transformJsonBody($request);
-      // $request = json_decode($request->getContent(), true);
-    return new JsonResponse($request->getContent(), 200, $headers = []);
-    return $request;
-
-    $request = $this->transformJsonBody($request);
-
-    if (! $request) {
-      // return $this->respondValidationError('Please provide a valid request!');
-      return 'Please provide a valid request!!';
-    }
-
-    // // validate the title
-    // if (! $request->get('title')) {
-    //   return $this->respondValidationError('Please provide a title!');
-    // }
-    if (! $request->get('name')) {
-      return 'Please provide a name!';
-    }
-    if (! $request->get('email')) {
-      return 'Please provide a email!';
-    }
-    if (! $request->get('message')) {
-      return 'Please provide a message!';
-    }
-
-    // persist the new entry
-    $entry = new Entry;
-    // $entry->setTitle($request->get('title'));
-    // $entry->setCount(0);
-    $entry->setName($request->get('name'));
-    $entry->setEmail($request->get('email'));
-    $entry->setMessage($request->get('message'));
-    $em->persist($entry);
-    $em->flush();
-
-    // return $this->respondCreated($entryRepository->transform($entry));
-    // return $entryRepository->transform($entry);
-    return $this->redirectToRoute('entries');
-
-
-  }
-
-  protected function transformJsonBody(\Symfony\Component\HttpFoundation\Request $request)
-  {
-    $data = json_decode($request->getContent(), true);
-
-    if ($data === null) {
-      return $request;
-    }
-
-    $request->request->replace($data);
-
-    return $request;
-  }
 }
