1a8639b8843ab36babcd3ee2c51a8136857f0564
ivan006 <ivan.copeland2015@gmail.com>
2021-02-07 12:22:52 +0200

    1

--- src/Controller/EntryController.php
+++ src/Controller/EntryController.php
@@ -1,18 +1,23 @@
 <?php
 namespace App\Controller;
 
-use App\Entity\Task;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
 use App\Entity\Entry;
 use App\Repository\EntryRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Annotation\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\JsonResponse;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-// use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+
 
 class EntryController extends AbstractController
 {
@@ -30,46 +35,77 @@ class EntryController extends AbstractController
   //   return new JsonResponse($entries, 200, $headers = []);
   // }
 
+  // /**
+  // * @Route("/entries", methods="GET")
+  // */
+
   /**
-  * @Route("/entries", methods="GET")
+  * @Route("/entries")
   */
-  public function index(EntryRepository $entryRepository)
+  // public function index(EntryRepository $entryRepository)
+  public function new(Request $request): Response
   {
-    // creates a task object and initializes some data for this example
-    $task = new Task();
-    $task->setTask('Write a blog post');
-    $task->setDueDate(new \DateTime('tomorrow'));
+
+    $task = new Entry();
+
 
     $form = $this->createFormBuilder($task)
-    ->add('task', TextType::class)
-    ->add('dueDate', DateType::class)
-    ->add('save', SubmitType::class, ['label' => 'Create Task'])
+    ->add('name', TextType::class)
+    ->add('email', TextType::class)
+    ->add('message', TextType::class)
+    ->add('save', SubmitType::class, ['label' => 'Create'])
     ->getForm();
 
-    // // ...
-    // $task = new Task();
-    // // ...
-    //
-    // $form = $this->createForm(TaskType::class, $task);
+    $form->handleRequest($request);
+    if ($form->isSubmitted() && $form->isValid()) {
+      // $form->getData() holds the submitted values
+      // but, the original `$entry` variable has also been updated
+      $entry = $form->getData();
 
-    return $this->render('Entity/create.html.twig', [
+      // ... perform some action, such as saving the entry to the database
+      // for example, if Entry is a Doctrine entity, save it!
+      $entityManager = $this->getDoctrine()->getManager();
+      $entityManager->persist($entry);
+      $entityManager->flush();
+
+      // $entry->setName($request->get('name'));
+      // $entry->setEmail($request->get('email'));
+      // $entry->setMessage($request->get('message'));
+      // $em->persist($entry);
+      // $em->flush();
+
+      // return $this->redirectToRoute('entries');
+      return $this->redirect('entry_success');
+    }
+
+    return $this->render('Entry/new.html.twig', [
       'form' => $form->createView(),
     ]);
 
-    // return $this->render('Entity/create.html.twig', [
-    //   // this array defines the variables passed to the template,
-    //   // where the key is the variable name and the value is the variable value
-    //   // (Twig recommends using snake_case variable names: 'foo_bar' instead of 'fooBar')
-    //   'user_first_name' => "123",
-    //   'notifications' => "123",
-    // ]);
+
+
   }
 
   /**
-  * @Route("/entries", methods="POST")
+  * @Route("/entry_success")
   */
+  public function entry_success(EntryRepository $entryRepository)
+  {
+
+    return $this->render('Entry/entry_success.html.twig', []);
+
+  }
+
+  // /**
+  // * @Route("/entries", methods="POST")
+  // */
   public function create(Request $request, EntryRepository $entryRepository, EntityManagerInterface $em)
   {
+    $request = $this->transformJsonBody($request);
+      // $request = json_decode($request->getContent(), true);
+    return new JsonResponse($request->getContent(), 200, $headers = []);
+    return $request;
+
     $request = $this->transformJsonBody($request);
 
     if (! $request) {
@@ -104,5 +140,20 @@ class EntryController extends AbstractController
     // return $this->respondCreated($entryRepository->transform($entry));
     // return $entryRepository->transform($entry);
     return $this->redirectToRoute('entries');
+
+
+  }
+
+  protected function transformJsonBody(\Symfony\Component\HttpFoundation\Request $request)
+  {
+    $data = json_decode($request->getContent(), true);
+
+    if ($data === null) {
+      return $request;
+    }
+
+    $request->request->replace($data);
+
+    return $request;
   }
 }
