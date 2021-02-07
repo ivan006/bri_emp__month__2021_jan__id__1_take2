bdc3c5da79347b227e36b46669b2fb2bcc095ef0
ivan006 <ivan.copeland2015@gmail.com>
2021-02-07 11:23:19 +0200

    1

--- templates/Entity/create.php
+++ templates/Entity/create.php
@@ -13,18 +13,24 @@
 
   <div class="container">
     <h2>Stacked form</h2>
-    <form action="/action_page.php">
+    {# templates/task/new.html.twig #}
+    {{ form(form) }}
+    <!-- <form action="/action_page.php">
+      <div class="form-group">
+        <label for="email">Name:</label>
+        <input type="email" class="form-control" name="email">
+      </div>
       <div class="form-group">
         <label for="email">Email:</label>
-        <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
+        <input type="email" class="form-control" name="email">
       </div>
       <div class="form-group">
         <label for="pwd">Password:</label>
-        <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pswd">
+        <input type="password" class="form-control" name="pswd">
       </div>
-      
+
       <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
+    </form> -->
   </div>
 
 </body>
